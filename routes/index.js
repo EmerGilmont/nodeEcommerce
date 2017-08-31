3d9e5d2dce7f4424e26184d67fc3e982581c09ca
Emer <emer0461@hotmail.com>
2017-08-31 14:01:19 +0200

    affichage des pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/inscription', function(req, res, next) {
+  res.render('inscription');
+});
+
+router.get('/connexion', function(req, res, next) {
+  res.render('connexion');
+});
+
+router.get('/connecter', function(req, res, next) {
+  res.render('connecter');
+});
+
 module.exports = router;
